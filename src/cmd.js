a94f7be1d4ad439d725375fbe7ddaaeaa443ec23
hMsats <millibitcoins@gmail.com>
2024-12-19 06:57:01 +0100

    spark now also works when xpay-as-pay=true

--- src/cmd.js
+++ src/cmd.js
@@ -90,7 +90,10 @@ export const commands = {
   // and return it with the invoice metadata.
 , async _pay(paystr, ...args) {
     this.emit('paying', paystr)
-    const pay_result = await this.pay(paystr, ...args)
+    // doesn't work when xpay-handle-pay=true
+    //const pay_result = await this.pay(paystr, ...args)
+    const pay_result = await this.pay(paystr)
+
     await attachInvoiceMeta(this, pay_result)
     return pay_result
   }
